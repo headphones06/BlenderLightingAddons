ebfe13d72780a22da153fbd7f2d8b6ac86d9fdac
unknown <ryouta.headphones@gmail.com>
2022-04-22 00:03:30 +0900

    complete develop
    This program is  completed.

--- lithing_set.py
+++ lithing_set.py
@@ -1,20 +1,20 @@
+import bpy
+from bpy.props import FloatVectorProperty, EnumProperty
+from mathutils import Vector
+import math
+
 bl_info = {
-    "name": "LLightingset",
+    "name": "Lightingset",
     "author": "headphones",
     "version": (0, 1, 0),
     "blender": (3, 10, 0),
     "location": "View3D > Add",
     "warning": "",
+    "support": 'TESTING',
     "description": "選択中のオブジェクトに対し、X軸正方向を正面としたライト設置アドオン",
     "doc_url": "",
     "category": "Object",
 }
-
-# ref: https://github.com/Cfyzzz/Other-scripts/blob/master/f2.py
-
-import bpy
-from bpy.props import FloatVectorProperty, EnumProperty
-from mathutils import Vector
 
 class Noperation(bpy.types.Operator):
 
@@ -28,22 +28,28 @@ class Noperation(bpy.types.Operator):
 
 class TWO_lightsetting(bpy.types.Operator):
 
-    bl_idname = "object.plactice_plus_duplicate"
-    bl_label = "Plus Duplicate"
-    bl_description = "Duplication by plus vectol"
+    bl_idname = "object.two_lights"
+    bl_label = "Two Lamps Lighting"
+    bl_description = "Add two lamps"
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
 
         active_obj = context.active_object
         #first light add
-        bpy.ops.object.light_add(type='Area',rotation=(60.0, 0.0, 45.0))
+        bpy.ops.object.light_add(type='AREA')
         active_light = context.active_object
-        active_light.location = active_obj.location + Vector((7.0, -7.0, 5.0))
+        active_light.location = active_obj.location + Vector((7.0, -7.0, 6.0))
+        active_light.rotation_euler.x = math.pi*1/3
+        active_light.rotation_euler.y = 0
+        active_light.rotation_euler.z = math.pi*1/4
         #second light add
-        bpy.ops.object.light_add(type='Area',rotation=(120.0, 0.0, 120.0))
+        bpy.ops.object.light_add(type='AREA')
         active_light = context.active_object
-        active_light.location = active_obj.location + Vector((7.0, 5.5, -1.0))
+        active_light.location = active_obj.location + Vector((3.8, 2, -1))
+        active_light.rotation_euler.x = math.pi*105/180
+        active_light.rotation_euler.y = 0
+        active_light.rotation_euler.z = math.pi*2/3
         
         self.report({'INFO'},
                     "Add two lights")
@@ -52,9 +58,9 @@ class TWO_lightsetting(bpy.types.Operator):
 
 class THREE_lightsetting(bpy.types.Operator):
 
-    bl_idname = "object.plactice_minus_duplicate"
-    bl_label = "Minus Duplicate"
-    bl_description = "Duplication by minus vectol"
+    bl_idname = "object.three_lights"
+    bl_label = "Three Lamps Lighting"
+    bl_description = "Add three lamps"
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
@@ -68,26 +74,22 @@ class THREE_lightsetting(bpy.types.Operator):
         print("Operator '{}' is executed".format(self.bl_idname))
         return {'FINISHED'}
 
-class DuplicatingMenu(bpy.types.Menu):
+class LighttingSetMenu(bpy.types.Menu):
 
     bl_idname = "PLACTICE_MT_CupMenu"
-    bl_label = "Dup Menu"
-    bl_description = "Menu for Duplication"
+    bl_label = "Lots Lighting"
+    bl_description = "Menu for Lightings"
 
-    # 引数
-    #   self: メニュークラスのインスタンス [メニュークラス]
-    #   context: drawメソッドが呼ばれたときのコンテキスト
-    #            [bpy.types.Context]
     def draw(self, context):
         layout = self.layout
         # メニュー項目の追加
-        layout.operator(TWO_lightsetting.bl_idname, text=("Two Lamp Lighting"))
-        layout.operator(Noperation.bl_idname, text=("Three Lamp Lighting"))
+        layout.operator(TWO_lightsetting.bl_idname, text=("Two Lamps Lighting"))
+        layout.operator(Noperation.bl_idname, text=("Three Lamps Lighting"))
 
 # メニューを構築する関数
 def menu_fn(self, context):
     self.layout.separator()
-    self.layout.menu(DuplicatingMenu.bl_idname)
+    self.layout.menu(LighttingSetMenu.bl_idname)
 
 
 # Blenderに登録するクラス
@@ -95,7 +97,7 @@ classes = [
     Noperation,
     TWO_lightsetting,
     THREE_lightsetting,
-    DuplicatingMenu,
+    LighttingSetMenu,
 ]
 
 
@@ -103,11 +105,11 @@ classes = [
 def register():
     for c in classes:
         bpy.utils.register_class(c)
-    bpy.types.VIEW3D_MT_object.append(menu_fn)
+    bpy.types.VIEW3D_MT_add.append(menu_fn)
 
 # アドオン無効化時の処理
 def unregister():
-    bpy.types.VIEW3D_MT_object.remove(menu_fn)
+    bpy.types.VIEW3D_MT_add.remove(menu_fn)
     for c in classes:
         bpy.utils.unregister_class(c)
 
